3ff8e018bec5467b641f4224e6529ca545dd3764
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2015-09-20 14:36:33 +0900

    add driver-side Parameter Server classes and client-side ParameterServerConfiguration

---
+++ src/main/java/edu/snu/reef/dolphin/ps/driver/ParameterServerDriver.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 2015 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.reef.dolphin.ps.driver;
+
+import edu.snu.reef.dolphin.ps.ParameterServerConfiguration.SerializedCodecConfiguration;
+import edu.snu.reef.dolphin.ps.ParameterServerConfiguration.SerializedUpdaterConfiguration;
+import edu.snu.reef.dolphin.ps.ns.NetworkContextRegister;
+import edu.snu.reef.dolphin.ps.ns.PSMessageHandler;
+import edu.snu.reef.dolphin.ps.server.ServerSideMsgHandler;
+import edu.snu.reef.dolphin.ps.worker.WorkerSideMsgHandler;
+import org.apache.reef.annotations.audience.DriverSide;
+import org.apache.reef.evaluator.context.parameters.ContextStartHandlers;
+import org.apache.reef.evaluator.context.parameters.ContextStopHandlers;
+import org.apache.reef.io.network.naming.NameServer;
+import org.apache.reef.io.network.naming.parameters.NameResolverNameServerAddr;
+import org.apache.reef.io.network.naming.parameters.NameResolverNameServerPort;
+import org.apache.reef.io.network.util.StringIdentifierFactory;
+import org.apache.reef.tang.Configuration;
+import org.apache.reef.tang.Tang;
+import org.apache.reef.tang.annotations.Parameter;
+import org.apache.reef.tang.formats.ConfigurationSerializer;
+import org.apache.reef.wake.IdentifierFactory;
+import org.apache.reef.wake.remote.address.LocalAddressProvider;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+/**
+ * Driver code for Parameter Server applications.
+ * Should be injected into the user's Driver class.
+ * Provides methods for getting context and service configurations.
+ */
+@DriverSide
+public final class ParameterServerDriver {
+
+  /**
+   * Manager object that represents the Parameter Server configuration to be used.
+   */
+  private final ParameterServerManager psManager;
+
+  /**
+   * Configuration for key/preValue/value {@link org.apache.reef.wake.remote.Codec}s.
+   */
+  private final Configuration codecConfiguration;
+
+  /**
+   * Configuration that specifies the {@link edu.snu.reef.dolphin.ps.server.ParameterUpdater} class to use.
+   */
+  private final Configuration updaterConfiguration;
+
+  /**
+   * Server for providing remote identifiers to the Network Connection Service.
+   */
+  private final NameServer nameServer;
+
+  /**
+   * Object needed for detecting the local address of this container.
+   */
+  private final LocalAddressProvider localAddressProvider;
+
+  @Inject
+  private ParameterServerDriver(final ParameterServerManager psManager,
+                                final ConfigurationSerializer configurationSerializer,
+                                @Parameter(SerializedCodecConfiguration.class) final String serializedCodecConf,
+                                @Parameter(SerializedUpdaterConfiguration.class) final String serializedUpdaterConf,
+                                final NameServer nameServer,
+                                final LocalAddressProvider localAddressProvider) throws IOException {
+    this.psManager = psManager;
+    this.codecConfiguration = configurationSerializer.fromString(serializedCodecConf);
+    this.updaterConfiguration = configurationSerializer.fromString(serializedUpdaterConf);
+    this.nameServer = nameServer;
+    this.localAddressProvider = localAddressProvider;
+  }
+
+  /**
+   * @return context configuration that both worker and server Evaluators uses
+   */
+  private Configuration getCommonContextConfiguration() {
+    return Tang.Factory.getTang().newConfigurationBuilder(codecConfiguration)
+        .bindSetEntry(ContextStartHandlers.class, NetworkContextRegister.RegisterContextHandler.class)
+        .bindSetEntry(ContextStopHandlers.class, NetworkContextRegister.UnregisterContextHandler.class)
+        .build();
+  }
+
+  /**
+   * @return context configuration for an Evaluator that uses a {@code ParameterWorker}
+   */
+  public Configuration getWorkerContextConfiguration() {
+    return Tang.Factory.getTang().newConfigurationBuilder(
+        getCommonContextConfiguration(),
+        psManager.getWorkerContextConfiguration())
+        .bindNamedParameter(PSMessageHandler.class, WorkerSideMsgHandler.class)
+        .build();
+
+  }
+
+  /**
+   * @return context configuration for an Evaluator that uses a {@code ParameterServer}
+   */
+  public Configuration getServerContextConfiguration() {
+    return Tang.Factory.getTang().newConfigurationBuilder(
+        getCommonContextConfiguration(),
+        psManager.getServerContextConfiguration(),
+        updaterConfiguration)
+        .bindNamedParameter(PSMessageHandler.class, ServerSideMsgHandler.class)
+        .build();
+  }
+
+  /**
+   * @return {@link NameServer} related configuration to be used by services
+   */
+  private Configuration getNameResolverServiceConfiguration() {
+    return Tang.Factory.getTang().newConfigurationBuilder()
+        .bindNamedParameter(NameResolverNameServerPort.class, Integer.toString(nameServer.getPort()))
+        .bindNamedParameter(NameResolverNameServerAddr.class, localAddressProvider.getLocalAddress())
+        .build();
+  }
+
+  /**
+   * @return service configuration for an Evaluator that uses a {@code ParameterWorker}
+   */
+  public Configuration getWorkerServiceConfiguration() {
+    return Tang.Factory.getTang().newConfigurationBuilder(
+        psManager.getWorkerServiceConfiguration(),
+        getNameResolverServiceConfiguration())
+        .bindImplementation(IdentifierFactory.class, StringIdentifierFactory.class)
+        .build();
+  }
+
+  /**
+   * @return service configuration for an Evaluator that uses a {@code ParameterServer}
+   */
+  public Configuration getServerServiceConfiguration() {
+    return Tang.Factory.getTang().newConfigurationBuilder(
+        psManager.getServerServiceConfiguration(),
+        getNameResolverServiceConfiguration())
+        .bindImplementation(IdentifierFactory.class, StringIdentifierFactory.class)
+        .build();
+  }
+}
---
+++ src/main/java/edu/snu/reef/dolphin/ps/driver/SingleNodeParameterServerManager.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2015 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.reef.dolphin.ps.driver;
+
+import edu.snu.reef.dolphin.ps.ns.EndpointId;
+import edu.snu.reef.dolphin.ps.server.ParameterServer;
+import edu.snu.reef.dolphin.ps.server.SingleNodeParameterServer;
+import edu.snu.reef.dolphin.ps.worker.ParameterWorker;
+import edu.snu.reef.dolphin.ps.worker.SingleNodeParameterWorker;
+import org.apache.reef.annotations.audience.DriverSide;
+import org.apache.reef.tang.Configuration;
+import org.apache.reef.tang.Tang;
+import org.apache.reef.tang.annotations.Name;
+import org.apache.reef.tang.annotations.NamedParameter;
+
+import javax.inject.Inject;
+
+/**
+ * Manager class for a Parameter Server that uses only one node for a server.
+ * This manager does NOT handle server or worker faults.
+ */
+@DriverSide
+public final class SingleNodeParameterServerManager implements ParameterServerManager {
+  private static final String SERVER_ID = "SINGLE_NODE_SERVER_ID";
+
+  @Inject
+  private SingleNodeParameterServerManager() {
+  }
+
+  /**
+   * Returns worker-side context configuration.
+   * Binds the server id to a named parameter so that workers know where to send push/pull messages to.
+   */
+  @Override
+  public Configuration getWorkerContextConfiguration() {
+    return Tang.Factory.getTang().newConfigurationBuilder()
+        .bindNamedParameter(ServerId.class, SERVER_ID)
+        .build();
+  }
+
+  /**
+   * Returns server-side context configuration.
+   * Binds the server id to a named parameter so that
+   * the server uses it to register itself to Network Connection Service.
+   */
+  @Override
+  public Configuration getServerContextConfiguration() {
+    return Tang.Factory.getTang().newConfigurationBuilder()
+        .bindNamedParameter(EndpointId.class, SERVER_ID)
+        .build();
+  }
+
+  /**
+   * Returns worker-side service configuration.
+   * Sets {@link SingleNodeParameterWorker} as the {@link ParameterWorker} class.
+   */
+  @Override
+  public Configuration getWorkerServiceConfiguration() {
+    return Tang.Factory.getTang().newConfigurationBuilder()
+        .bindImplementation(ParameterWorker.class, SingleNodeParameterWorker.class)
+        .build();
+  }
+
+  /**
+   * Returns server-side service configuration.
+   * Sets {@link SingleNodeParameterServer} as the {@link ParameterWorker} class.
+   */
+  @Override
+  public Configuration getServerServiceConfiguration() {
+    return Tang.Factory.getTang().newConfigurationBuilder()
+        .bindImplementation(ParameterServer.class, SingleNodeParameterServer.class)
+        .build();
+  }
+
+  @NamedParameter(doc = "server identifier for Network Connection Service")
+  public final class ServerId implements Name<String> {
+  }
+}
