7cd354d0f8f6fa05ef17e306cb417be30cd59b74
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2015-09-20 14:35:44 +0900

    add server-side Parameter Server classes

---
+++ src/main/java/edu/snu/reef/dolphin/ps/server/SingleNodeParameterServer.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2015 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.reef.dolphin.ps.server;
+
+import org.apache.reef.annotations.audience.EvaluatorSide;
+
+import javax.annotation.concurrent.ThreadSafe;
+import javax.inject.Inject;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Logger;
+
+/**
+ * Parameter Server server that consists of exactly one node.
+ * Users should spawn a single evaluator for this server.
+ */
+@EvaluatorSide
+@ThreadSafe
+public final class SingleNodeParameterServer<K, P, V> implements ParameterServer<K, P, V> {
+  private static final Logger LOG = Logger.getLogger(SingleNodeParameterServer.class.getName());
+
+  /**
+   * A map holding keys and values sent from workers.
+   */
+  private final ConcurrentMap<K, ValueEntry<V>> kvStore;
+
+  /**
+   * Object for processing preValues and applying updates to existing values.
+   */
+  private final ParameterUpdater<K, P, V> parameterUpdater;
+
+  @Inject
+  private SingleNodeParameterServer(final ParameterUpdater<K, P, V> parameterUpdater) {
+    this.kvStore = new ConcurrentHashMap<>();
+    this.parameterUpdater = parameterUpdater;
+  }
+
+  /**
+   * Store a {@code preValue} sent from a worker.
+   * Uses {@link ParameterUpdater} to generate a value from {@code preValue} and to apply the generated value to
+   * the k-v store. Updates are done only when a write lock is acquired, for concurrency between
+   * multiple push/pull threads.
+   * @param key key object that {@code preValue} is associated with
+   * @param preValue preValue sent from the worker
+   */
+  @Override
+  public void push(final K key, final P preValue) {
+    if (!kvStore.containsKey(key)) {
+      kvStore.putIfAbsent(key, new ValueEntry<>(parameterUpdater.initValue(key)));
+    }
+
+    final V deltaValue = parameterUpdater.process(key, preValue);
+    if (deltaValue == null) {
+      return;
+    }
+
+    final ValueEntry<V> oldValueEntry = kvStore.get(key);
+    oldValueEntry.getReadWriteLock().writeLock().lock();
+    try {
+      // We cannot predict whether the updater will modify the value object itself or not.
+      // Therefore we acquire the lock before the update starts.
+      // If we can assume value objects are immutable then we might not even need a lock,
+      // since the k-v store is already a ConcurrentMap.
+      final V newValue = parameterUpdater.update(oldValueEntry.getValue(), deltaValue);
+      oldValueEntry.setValue(newValue);
+    } finally {
+      oldValueEntry.getReadWriteLock().writeLock().unlock();
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ValueEntry<V> pull(final K key) {
+    if (!kvStore.containsKey(key)) {
+      kvStore.putIfAbsent(key, new ValueEntry<>(parameterUpdater.initValue(key)));
+    }
+
+    return kvStore.get(key);
+  }
+}
