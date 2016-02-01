4ad308422bfc618e39e39a0b07641dcd5980fc30
seyounglee <fatalu1226@naver.com>
2016-02-01 10:59:07 +0900

    Feedforward / backpropagating function support reshaping

--- dolphin-dnn/src/main/java/edu/snu/dolphin/dnn/layers/PoolingLayer.java
+++ dolphin-dnn/src/main/java/edu/snu/dolphin/dnn/layers/PoolingLayer.java
@@ -58,7 +58,8 @@ public final class PoolingLayer extends LayerBase {
                        @Parameter(StrideWidth.class) final int strideWidth,
                        @Parameter(KernelHeight.class) final int kernelHeight,
                        @Parameter(KernelWidth.class) final int kernelWidth,
-                       final LayerParameterInitializer layerParameterInitializer) {
+                       final LayerParameterInitializer layerParameterInitializer,
+                       final MatrixFactory matrixFactory) {
     super(index, inputShape);
     this.strideHeight = strideHeight;
     this.strideWidth = strideWidth;
@@ -66,6 +67,7 @@ public final class PoolingLayer extends LayerBase {
     this.kernelWidth = kernelWidth;
     this.outputShape = layerParameterInitializer.getOutputShape();
     this.poolingType = PoolType.valueOf(poolingType);
+    this.matrixFactory = matrixFactory;
   }
 
   @Override
@@ -85,27 +87,36 @@ public final class PoolingLayer extends LayerBase {
    * @return the output values for this layer.
    */
   private Matrix feedForwardMaxPooling(final Matrix input) {
-    final Matrix output = matrixFactory.create(outputShape[0], outputShape[1]);
-    indexMatrix = matrixFactory.create(outputShape[0], outputShape[1]);
-    int ih = 0, iw = 0;
-    for (int oh = 0; oh < outputShape[0]; ++oh, ih += strideHeight) {
-      for (int ow = 0; ow < outputShape[1]; ++ow, iw += strideWidth) {
-        //Find maximum value within kernel range and put it in the output matrix.
-        float max = input.get(ih, iw);
-        int index = iw + ih * input.getColumns();
-        for (int kh = 0; kh < kernelHeight; ++kh) {
-          for (int kw = 0; kw < kernelWidth; ++kw) {
-            final float tempValue = input.get(ih + kh, iw + kw);
-            if (tempValue > max) {
-              max = tempValue;
-              index = iw + kw + (ih + kh) * input.getColumns();
+    final int[] inputShape = getInputShape();
+    final Matrix output = matrixFactory.create(outputShape[0] * outputShape[1], input.getColumns());
+    indexMatrix = matrixFactory.create(outputShape[0] * outputShape[1], input.getColumns());
+    for (int n = 0; n < input.getColumns(); ++n) {
+      final Matrix inputImage = input.getColumn(n).reshape(inputShape[0], inputShape[1]).transpose();
+      final Matrix outputImage = matrixFactory.create(outputShape[0], outputShape[1]);
+      final Matrix indexMatrixImage = matrixFactory.create(outputShape[0], outputShape[1]);
+      int ih = 0;
+      for (int oh = 0; oh < outputShape[0]; ++oh, ih += strideHeight) {
+        int iw = 0;
+        for (int ow = 0; ow < outputShape[1]; ++ow, iw += strideWidth) {
+          //Find maximum value within kernel range and put it in the output matrix.
+          float max = inputImage.get(ih, iw);
+          int index = iw + ih * inputImage.getColumns();
+          for (int kh = 0; kh < kernelHeight; ++kh) {
+            for (int kw = 0; kw < kernelWidth; ++kw) {
+              final float tempValue = inputImage.get(ih + kh, iw + kw);
+              if (tempValue > max) {
+                max = tempValue;
+                index = iw + kw + (ih + kh) * inputImage.getColumns();
+              }
             }
           }
+          outputImage.put(oh, ow, max);
+          //Save index of max value.
+          indexMatrixImage.put(oh, ow, index);
         }
-        output.put(oh, ow, max);
-        //Save index of max value.
-        indexMatrix.put(oh, ow, index);
       }
+      output.putColumn(n, outputImage.transpose().reshape(outputShape[0] * outputShape[1], 1));
+      indexMatrix.putColumn(n, indexMatrixImage.transpose().reshape(outputShape[0] * outputShape[1], 1));
     }
     return output;
   }
@@ -116,20 +127,27 @@ public final class PoolingLayer extends LayerBase {
    * @return the output values for this layer.
    */
   private Matrix feedForwardAveragePooling(final Matrix input) {
+    final int[] inputShape = getInputShape();
     final int kernelSize = kernelHeight * kernelWidth;
-    final Matrix output = matrixFactory.create(outputShape[0], outputShape[1]);
-    int ih = 0, iw = 0;
-    for (int oh = 0; oh < outputShape[0]; ++oh, ih += strideHeight) {
-      for (int ow = 0; ow < outputShape[1]; ++ow, iw += strideWidth) {
-        //Find sum of values within kernel range and put average value in the output matrix.
-        int sum = 0;
-        for (int kh = 0; kh < kernelHeight; ++kh) {
-          for (int kw = 0; kw < kernelWidth; ++kw) {
-            sum += input.get(ih + kh, iw + kw);
+    final Matrix output = matrixFactory.create(outputShape[0] * outputShape[1], input.getColumns());
+    for (int n = 0; n < input.getColumns(); ++n) {
+      final Matrix inputImage = input.getColumn(n).reshape(inputShape[0], inputShape[1]).transpose();
+      final Matrix outputImage = matrixFactory.create(outputShape[0], outputShape[1]);
+      int ih = 0;
+      for (int oh = 0; oh < outputShape[0]; ++oh, ih += strideHeight) {
+        int iw = 0;
+        for (int ow = 0; ow < outputShape[1]; ++ow, iw += strideWidth) {
+          //Find sum of values within kernel range and put average value in the output matrix.
+          float sum = 0;
+          for (int kh = 0; kh < kernelHeight; ++kh) {
+            for (int kw = 0; kw < kernelWidth; ++kw) {
+              sum += inputImage.get(ih + kh, iw + kw);
+            }
           }
+          outputImage.put(oh, ow, sum / kernelSize);
         }
-        output.put(oh, ow, sum / kernelSize);
       }
+      output.putColumn(n, outputImage.transpose().reshape(outputShape[0] * outputShape[1], 1));
     }
     return output;
   }
@@ -160,14 +178,20 @@ public final class PoolingLayer extends LayerBase {
    */
   private Matrix backPropagateMaxPooling(final Matrix input, final Matrix nextError) {
     final Matrix error = matrixFactory.zeros(input.getRows(), input.getColumns());
-    for (int oh = 0; oh < nextError.getRows(); ++oh) {
-      for (int ow = 0; ow < nextError.getColumns(); ++ow) {
-        final int ih = (int) indexMatrix.get(oh, ow) / input.getColumns();
-        final int iw = (int) indexMatrix.get(oh, ow) - ih * input.getColumns();
-        final float tempError = nextError.get(oh, ow) + error.get(ih, iw);
-        //Add error to saved index.
-        error.put(ih, iw, tempError);
+    final int[] inputShape = getInputShape();
+    for (int n = 0; n < input.getColumns(); ++n) {
+      final Matrix errorImage = matrixFactory.zeros(inputShape[0], inputShape[1]);
+      final Matrix nextErrorImage = nextError.getColumn(n).reshape(outputShape[0], outputShape[1]).transpose();
+      for (int oh = 0; oh < outputShape[0]; ++oh) {
+        for (int ow = 0; ow < outputShape[1]; ++ow) {
+          final int ih = (int) indexMatrix.get(oh * outputShape[1] + ow, n) / errorImage.getColumns();
+          final int iw = (int) indexMatrix.get(oh * outputShape[1] + ow, n) - ih * errorImage.getColumns();
+          final float tempError = nextErrorImage.get(oh, ow) + errorImage.get(ih, iw);
+          //Add error to saved index.
+          errorImage.put(ih, iw, tempError);
+        }
       }
+      error.putColumn(n, errorImage.transpose().reshape(inputShape[0] * inputShape[1], 1));
     }
     return error;
   }
@@ -179,20 +203,26 @@ public final class PoolingLayer extends LayerBase {
    * @return errors for this layer with the specified input value.
    */
   private Matrix backPropagateAveragePooling(final Matrix input, final Matrix nextError) {
-    final int kernelSize = kernelHeight * kernelWidth;
+    final float kernelSize = kernelHeight * kernelWidth;
     final Matrix error = matrixFactory.zeros(input.getRows(), input.getColumns());
-    for (int oh = 0; oh < nextError.getRows(); ++oh) {
-      for (int ow = 0; ow < nextError.getColumns(); ++ow) {
-        final int sh = strideHeight * oh;
-        final int sw = strideWidth * ow;
-        for (int ih = sh; ih < sh + kernelHeight; ++ih) {
-          for (int iw = sw; iw < sw + kernelWidth; ++iw) {
-            //Add error divided by kernel size for all pixels within the range.
-            final float tempError = nextError.get(oh, ow) / kernelSize + error.get(ih, iw);
-            error.put(ih, iw, tempError);
+    final int[] inputShape = getInputShape();
+    for (int n = 0; n < input.getColumns(); ++n) {
+      final Matrix errorImage = matrixFactory.zeros(inputShape[0], inputShape[1]);
+      final Matrix nextErrorImage = nextError.getColumn(n).reshape(outputShape[0], outputShape[1]).transpose();
+      for (int oh = 0; oh < outputShape[0]; ++oh) {
+        for (int ow = 0; ow < outputShape[1]; ++ow) {
+          final int sh = strideHeight * oh;
+          final int sw = strideWidth * ow;
+          for (int ih = sh; ih < sh + kernelHeight; ++ih) {
+            for (int iw = sw; iw < sw + kernelWidth; ++iw) {
+              //Add error divided by kernel size for all pixels within the range.
+              final float tempError = nextErrorImage.get(oh, ow) / kernelSize + errorImage.get(ih, iw);
+              errorImage.put(ih, iw, tempError);
+            }
           }
         }
       }
+      error.putColumn(n, errorImage.transpose().reshape(inputShape[0] * inputShape[1], 1));
     }
     return error;
   }
